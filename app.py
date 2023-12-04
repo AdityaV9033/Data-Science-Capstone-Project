6afb75e554bf1ec4a267fe45b92e88ef1f8bb9c3
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-04 16:12:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ print(X.shape)
 print(y.shape)
 print(type(X))
 print(type(y))
-import subprocess
-subprocess.run(["pip", "install", "-U", "scikit-learn"])
+import streamlit as st
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
