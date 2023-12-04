9a194a85d4e9f57daca7e4dbd89aa7311651fa26
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-04 18:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ print(X.shape)
 print(y.shape)
 print(type(X))
 print(type(y))
-import sklearn
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
