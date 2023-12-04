371774db78e05e5ebc63f61fc14d7bab6082b198
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-04 18:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ print(X.shape)
 print(y.shape)
 print(type(X))
 print(type(y))
-import streamlit as st
+import sklearn
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
